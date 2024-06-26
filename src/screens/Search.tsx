00e120413e0df51ebc2266bcc13d5437b0cbdc5b
ninditobing7 <ninditobing7@gmail.com>
2024-06-26 10:41:41 +0700

    Progress dari Nindi

---
+++ src/screens/Search.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+export default function Search(): JSX.Element {
+  return (
+    <View>
+      <Text>Search</Text>
+    </View>
+  )
+}
